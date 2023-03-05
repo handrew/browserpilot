159f6e5236b38e5831640a22a54bbe31d02d456c
Andrew Han <handrew11@gmail.com>
2023-03-05 13:52:23 -0800

    add cli arg for run_studio

--- run_studio.py
+++ run_studio.py
@@ -1,4 +1,10 @@
+import sys
 from browserpilot.studio import Studio
 
-studio = Studio(chromedriver_path="./chromedriver")
+if len(sys.argv) >= 2:
+    instructions = sys.argv[1]
+else:
+    instructions = None
+
+studio = Studio(instructions_to_load=instructions, chromedriver_path="./chromedriver")
 studio.run()
